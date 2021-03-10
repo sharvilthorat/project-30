8f45153eab2014a9975753eb2d6bf274664859a4
sharvilthorat <74446962+sharvilthorat@users.noreply.github.com>
2021-03-10 16:55:36 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,45 @@
+class Block{
+    constructor(x, y, width, height) {
+      this.visiblity=255;
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        if(this.body.speed<3){
+          push();
+          var angle = this.body.angle;
+          var pos= this.body.position;
+     
+          translate(pos.x, pos.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(0,0,this.width, this.height);
+          pop();
+      
+}
+
+else{
+  push();
+  World.remove(world,this.body)
+  this.visiblity=this.visiblity-5
+  tint(255,this.visiblity)
+  rect(this.image,this.body.position.x,this.body.position.y,50,50);
+pop();
+}
+
+
+  }
+};
+
+
+
+ 
